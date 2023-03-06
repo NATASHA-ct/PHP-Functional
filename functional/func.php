8371d5ca6397f52f710fddbe41e7da735979ab37
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-06 10:19:46 +0200

    Add constants

--- functional/func.php
+++ functional/func.php
@@ -4,9 +4,7 @@
 
 // functions
 function say_Greet(){
-    echo "Hey tascha!   " ;
-
-    echo "Hope you are great!   ";
+    echo "Hey tascha!   " . "<br>" . "Hope you are great!   ";
 }
 
 say_Greet();
@@ -16,12 +14,34 @@ say_Greet();
 
 
 function say_all(){
-  say_Greet();
-
-  echo "this is it";
+  say_Greet() ;
+  
+  echo "<br>" . "this is it  >";
 }
 
 say_all();
 
 
+// Global variables
 
+$var = "Im a global variable!"; 
+
+function convert(){
+    // using a global variable in a function
+  global $var;
+  echo $var  ."<br>" ;
+
+// local variable
+  $home = "Im a local variable";
+    echo $home;
+}
+
+convert();
+
+// CONSTANTS
+// constants are like variables but they cannot be changed
+
+define("NAME", "Tascha");
+echo "<br>" . NAME;
+
+?>
