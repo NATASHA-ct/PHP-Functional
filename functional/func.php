daa8aadf49849a7d418682e962a1ad55748da409
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-06 09:55:14 +0200

    Modify functions

--- functional/func.php
+++ functional/func.php
@@ -1,3 +1,27 @@
 <?php
+// associative array haave specific key names not just index numbers
 
-?>
+
+// functions
+function say_Greet(){
+    echo "Hey tascha!   " ;
+
+    echo "Hope you are great!   ";
+}
+
+say_Greet();
+
+// calling a function inside another function
+
+
+
+function say_all(){
+  say_Greet();
+
+  echo "this is it";
+}
+
+say_all();
+
+
+
