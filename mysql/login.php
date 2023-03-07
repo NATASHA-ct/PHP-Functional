9e550f8f464340c42aec9d8a199be0b162084411
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-07 18:41:12 +0200

    Check DB connection

--- mysql/login.php
+++ mysql/login.php
@@ -22,6 +22,15 @@
         //use mysqli api to connect to the  DB
         $connect = mysqli_connect('locahost', 'root','','loginapp'); 
        
+        //check if the connection was successful
+        if($connect){
+            echo "We are connected";
+        }
+        else{
+            //die is a function that stops any other code from running and it returns error in the paranthesis
+            die("Database connection failed");
+        }
+
      }
 
 ?>
