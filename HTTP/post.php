001283b1b1d7cc7b93939e4308dbc409ebbab6c9
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-08 17:32:50 +0200

    Add the super global

---
+++ HTTP/post.php
@@ -0,0 +1,35 @@
+<?php
+     echo $_POST['submit'];
+      
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class='col-xs-6'>
+    <form action='post.php' method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control">
+        </div>
+        <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+    </form>
+
+</div>
+
+
+</body>
+</html>
