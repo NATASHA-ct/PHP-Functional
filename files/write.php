379e95a16278fa627af1312b1622da3882522e82
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-09 23:51:25 +0200

    Add write

---
+++ files/write.php
@@ -0,0 +1,21 @@
+<?php
+
+// to create a file first create name
+$file = "e.g.txt";
+
+// use fopen function to open it pass name and what we want to do with it.
+if($handle = fopen($file, "w")){
+// write to file
+fwrite($handle, "php is fun");
+
+fclose($handle);
+
+}else{
+ echo "Could not write files";
+}
+
+// close file after to speed up application
+
+
+// linux or mac users need to give apache permission to write using change ownership  (chown) check on stackoverflow
+?>
