8e37aaf789bb31f54ae89c907bbb62aa74aad27a
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-09 11:56:35 +0200

    This keyword

--- OOP/CAR.php
+++ OOP/CAR.php
@@ -3,11 +3,16 @@
 // creating a class
 class Car{
     // properties of a car
+   var $wheels = 5;
+   var $hood = 1;
+  var   $engine = 1;
+   var $doors = 4;
 
-   
     // creating a function
      function MoveWheels(){
         echo "Wheels move";
+        // changing val for wheels
+        $this->wheels = 10;
      }
 }
 
@@ -23,7 +28,7 @@ if (method_exists("Car","MoveWheels")){
     echo "I can move";
 
 }
-
+echo "<br>";
 // create car object
 
 $bmw = new Car();
@@ -31,5 +36,8 @@ $bmw = new Car();
 // call the function on object
 
 $bmw->MoveWheels();
+echo "<br>";
+// echo the value of wheels
+echo $bmw->wheels;
 
 ?>
