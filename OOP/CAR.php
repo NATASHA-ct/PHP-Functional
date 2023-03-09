98c164ff8c14b8e1636db0e043b7dc90fc3fdbc6
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-09 11:32:11 +0200

    Check for method

--- OOP/CAR.php
+++ OOP/CAR.php
@@ -3,7 +3,7 @@
 // creating a class
 class Car{
     // properties of a car
-    
+
    
     // creating a function
      function MoveWheels(){
@@ -14,6 +14,13 @@ class Car{
 // using built in function to check if it exist.
 if (class_exists("Car")){
     echo "yeaaaaa  ahhhhhhh";
+
+}
+
+echo "<br>";
+// using built in function to check if it exist.
+if (method_exists("Car","MoveWheels")){
+    echo "I can move";
 
 }
 
